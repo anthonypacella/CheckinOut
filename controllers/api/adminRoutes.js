e23c23a099a2e2fb05f04c85ce75e088c848bc88
anthonypacella <anthonyapacella@gmail.com>
2022-04-06 21:11:14 -0400

    added admin post routes

---
+++ controllers/api/adminRoutes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const { Administrator } = require('../../models/Administrator');
+const { Book } = require('../../models/Book');
+const { Branch } = require('../../model/Branch');
+const { CD } = require('../../models/CD');
+const { Movie } = require('../../models/Movie');
+const { Patron } = require('../../models/Patron');
+
+router.post('/add/administrator', async (req, res) => {
+    try {
+        const administratorData = await Administrator.create(req.body); 
+        res.status(200).json(administratorData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/add/book', async (req, res) => {
+    try {
+        const bookData = await Book.create(req.body); 
+        res.status(200).json(bookData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/add/branch', async (req, res) => {
+    try {
+        const branchData = await Branch.create(req.body); 
+        res.status(200).json(branchData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/add/cd', async (req, res) => {
+    try {
+        const cdData = await CD.create(req.body); 
+        res.status(200).json(cdData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/add/movie', async (req, res) => {
+    try {
+        const movieData = await Movie.create(req.body); 
+        res.status(200).json(movieData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/add/patron', async (req, res) => {
+    try {
+        const patronData = await Patron.create(req.body); 
+        res.status(200).json(patronData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+

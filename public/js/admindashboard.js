4e807e7d7f73463e2d5da45d89dbcb36c672198f
anthonypacella <anthonyapacella@gmail.com>
2022-04-09 13:11:51 -0400

    admin dashboard html added

---
+++ public/js/admindashboard.js
@@ -0,0 +1,11 @@
+const addAdminEl = document.querySelector('#addAdminbtn');
+const addPatronEl = document.querySelector('#addPatronbtn');
+const addBookEl = document.querySelector('#addBookbtn');
+
+
+
+addAdminEl.addEventListener('click', function() {
+
+
+
+}) 

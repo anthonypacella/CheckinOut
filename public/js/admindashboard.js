2fed385bfc0e56f980b9ab0386b7f94ea1f99755
anthonypacella <anthonyapacella@gmail.com>
2022-04-11 20:44:42 -0400

    updating admin dashboard

--- public/js/admindashboard.js
+++ public/js/admindashboard.js
@@ -2,11 +2,27 @@ const addAdminEl = document.querySelector('#addAdminbtn');
 const addPatronEl = document.querySelector('#addPatronbtn');
 const addBookEl = document.querySelector('#addBookbtn');
 
-addAdminEl.addEventListener('click', function() {
-    const adminUsername = $('#adminUsername').val();
-}) 
 
-addPatronEl.addEventListener('click', function() {
+
+const addAdministrator = async (event) => {
+    event.preventDefault();
+
+    const adminUsername = $('#adminUsername').val();
+
+    const response = await fetch('/api/add/administrator', {
+        method: 'POST',
+        body: JSON.stringify({adminUsername}),
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    if (response.ok) {
+        document.location.replace('/')
+    }
+}
+
+const addPatron = async (event) => {
+    event.preventDefault();
+
     const patronUsername = $('#patronUsername').val();
     const patronFirstName = $('#patronFirstName').val();
     const patronMiddleInitial = $('#patronMiddleInitial').val();
@@ -17,7 +33,7 @@ addPatronEl.addEventListener('click', function() {
     const patronState = $('#patronState').val();
     const patronZip = $('#patronZip').val();
 
-    const response = await fetch('/api/add/patron', {
+    const response = fetch('/api/add/patron', {
         method: 'POST',
         body: JSON.stringify({patronUsername, patronFirstName, patronMiddleInitial, patronLastName, patronAddressLine1, patronAddressLine2, patronCity, patronState, patronZip}),
         headers: {'Content-Type': 'application/json'},
@@ -27,9 +43,12 @@ addPatronEl.addEventListener('click', function() {
         document.location.replace('/')
     }
 
-}) 
+}
 
-addBookEl.addEventListener('click', function() {
+
+const addBook = async (event) => {
+    event.preventDefault();
+
     const bookISBN = $('#bookISBN').val();
     const bookTitle = $('#bookTitle').val();
     const bookAuthor = $('#bookAuthor').val();
@@ -38,4 +57,18 @@ addBookEl.addEventListener('click', function() {
     const bookPrice = $('#bookPrice').val();
     const bookSubject = $('#bookSubject').val();
     const bookDescription = $('#bookDescription').val();
-}) 
+
+    const response = fetch('/api/add/book', {
+        method: 'POST',
+        body: JSON.stringify({bookISBN, bookTitle, bookAuthor, bookYearPublished, bookEdition, bookPrice, bookSubject, bookDescription}),
+        headers: {'Content-Type': 'application/json'},
+    });
+
+    if (response.ok) {
+        document.location.replace('/')
+    }
+}
+
+addAdminEl.addEventListener('click', addAdministrator);
+addPatronEl.addEventListener('click', addPatron);
+addBookEl.addEventListener('click', addBook);

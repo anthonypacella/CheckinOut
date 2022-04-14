90bd5272d31ec05a9171c89a812089975a99336d
sally <sallyhoney96@gmail.com>
2022-04-14 07:12:25 -0400

    update user authentication

--- models/index.js
+++ models/index.js
@@ -2,4 +2,13 @@ const User = require('./User');
 const Book = require('./Book');
 const Search = require('./Search');
 
+User.hasMany(Search, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+Search.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Book, Search };

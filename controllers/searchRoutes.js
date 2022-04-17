2acfd94c76528c553904e371b9031898657fa290
sally <sallyhoney96@gmail.com>
2022-04-16 21:47:48 -0400

    update log in button

--- controllers/searchRoutes.js
+++ controllers/searchRoutes.js
@@ -4,7 +4,8 @@ const { Search } = require('../models');
 router.get('/', (req, res) => {
   if (req.session.logged_in) {
     console.log('req', req);
-    res.render('search', { message: `success` });
+    res.render('search', {       logged_in: req.session.logged_in 
+    });
   }
   else {
     res.redirect('/login');
